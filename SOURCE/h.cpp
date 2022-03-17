c6a89bda9af18fad48c8b3b839ae40cb833e63a0
Albert <20127370@student.hcmus.edu.vn>
2022-03-17 17:26:26 +0700

    almost finished

--- SOURCE/h.cpp
+++ SOURCE/h.cpp
@@ -1,102 +1,88 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
  
 static int cX[8] = {1,1,2,2,-1,-1,-2,-2};
 static int cY[8] = {2,-2,1,-1,2,-2,1,-1};
 
-bool checkLimitsWarnsdorff(int x, int y, int size){
-    return x >= 0 && y >= 0 && x < size && y < size;
-}
-
-bool checkEmptyWarnsdorff(int* a, int size, int x, int y){
-    return checkLimitsWarnsdorff(x, y, size) && a[y*size+x] < 0;
+bool checkEmptyWarnsdorff(int** board, int size, int x, int y){
+    return x >= 0 && y >= 0 && x < size && y < size && board[x][y] == -1;
 }
  
-/* So luong square con lai */
-int getDegreeWarnsdorff(int* &a, int size, int x, int y){
+int getDegreeWarnsdorff(int** board, int size, int x, int y){
     int count = 0;
-    for (int i = 0; i < size; i++){
-        if(checkEmptyWarnsdorff(a, size, x + cX[i], y + cY[i])) count++;
+    for (int i = 0; i < 8; i++){
+        if(checkEmptyWarnsdorff(board, size, x + cX[i], y + cY[i])) count++;
     }
     return count;
 }
- 
-// Picks next point using Warnsdorff's heuristic.
-// Returns false if it is not possible to pick next point.
-bool checkNextMoveWarnsdorff(int* &a, int size, int *x, int *y){
-    int minDegIdx = -1, c, minDeg = size + 1, nX, nY;
- 
-    // Try all N adjacent of (*x, *y) starting from a random adjacent. Find the adjacent with minimum degree.
-    int start = rand() % size;
-    for (int count = 0; count < size; count++){
-        int i = (start + count) % size;
-        nX = *x + cX[i];
-        nY = *y + cY[i];
-        c = getDegreeWarnsdorff(a, size, nX, nY);
-        if (checkEmptyWarnsdorff(a, size, nX, nY) && c < minDeg) {
+
+bool checkNextMoveWarnsdorff(int** board, int size, int* x, int* y){
+    int minDegIdx = -1, count, minDeg = size + 1, nextX, nextY;
+    for (int i = 0; i < 8; i++){
+        nextX = *x + cX[i];
+        nextY = *y + cY[i];
+        count = getDegreeWarnsdorff(board, size, nextX, nextY);
+        if (checkEmptyWarnsdorff(board, size, nextX, nextY) && count < minDeg) {
             minDegIdx = i;
-            minDeg = c;
+            minDeg = count;
         }
     }
-
     if (minDegIdx == -1) return false;
-    // Store coordinates of next point
-    nX = *x + cX[minDegIdx];
-    nY = *y + cY[minDegIdx];
-    // Mark next move
-    a[nY * size + nX] = a[*y * size + *x] + 1;
-    // Update next point
-    *x = nX;
-    *y = nY;
+    nextX = *x + cX[minDegIdx];
+    nextY = *y + cY[minDegIdx];
+    board[nextX][nextY] = board[*x][*y] + 1;
+    *x = nextX;
+    *y = nextY;
+
     return true;
 }
- 
-/* displays the chessboard with all the legal knight's moves */
-void printSolutionWarnsdorff(int* a, int size){
-    for (int i = 0; i < size; ++i){
-        for (int j = 0; j < size; ++j)
-            std::cout << a[j*size+i] << "\t";
-        std::cout << "\n";
+
+void outputFile(int** board, int size, int x, int y, int total, std::chrono::duration<double> elapsed_seconds){
+    std::fstream f("20127370_heuristic.txt", std::fstream::out);
+    f << x << " " << y << " " << size << "\n";
+    f << total << "\n";
+    f << elapsed_seconds.count() << "\n";
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            f << " " << std::setw(2) << board[i][j] << " ";
+        }
+        f << "\n";
     }
 }
  
-/* checks its neighbouring squares */
-/* If the knight ends on a square that is one knight's move from the beginning square,
-   then tour is closed */
-bool checkNeighbour(int size, int x, int y, int xx, int yy){
-    for (int i = 0; i < size; i++)
-        if (x+cX[i] == xx && y + cY[i] == yy) return true;
-    return false;
-}
- 
-/* Generates the legal moves using warnsdorff's heuristics. Returns false if not possible */
-bool findClosedTourWarnsdorff(){
-    int size;
-    std::cout << "Size: ";
-    std::cin >> size;
-    int* a = new int[size*size];
-    for (int i = 0; i< size * size; i++)
-        a[i] = -1;
-    
-    int sX = rand() % size;
-    int sY = rand() % size;
-    // Current points are same as initial points
-    int x = sX, y = sY;
-    a[y * size + x] = 1;
- 
-    // Keep picking next points using Warnsdorff's heuristic
-    for (int i = 0; i < size * size-1; ++i){
-        if (checkNextMoveWarnsdorff(a, size, &x, &y) == 0) return false;
+void findClosedTourWarnsdorff(int argc, char* argv[]){
+    int size = std::atoi(argv[6]);
+    int ** board = new int*[size];
+    for(int i = 0; i < size; i++){
+        board[i] = new int[size];
     }
- 
-    if (!checkNeighbour(size, x, y, sX, sY)) return false;
-    printSolutionWarnsdorff(a, size);
-    delete[] a;
-    return true;
+    int x, y, total, startX, startY;
+    auto start = std::chrono::system_clock::now();
+    while(true){
+        x = std::atoi(argv[2]);
+        y = std::atoi(argv[4]);
+        for(int i = 0; i < size; i++)
+            for(int j = 0; j < size; j++)
+                board[i][j] = -1;
+        total = 1;
+        startX = --x;
+        startY = --y;
+        board[x][y] = 1;
+        for (int i = 0; i < size * size-1; i++)
+            if (!checkNextMoveWarnsdorff(board, size, &x, &y)) continue;
+        break;
+    }
+    auto end  = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    outputFile(board, size, startX, startY, total, elapsed_seconds);
+
+    for(int i = 0; i < size; i++)
+        delete[] board[i];
+    delete[] board;
 }
  
-// Driver code
 int main(int argc, char* argv[]){
-    srand(time(NULL));
-    while (!findClosedTourWarnsdorff()){;}
+    findClosedTourWarnsdorff(argc, argv);
     return 0;
 }
--- SOURCE/main.cpp
+++ SOURCE/main.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
- 
 bool isSafeBacktracking(int x, int y, int** board, int size){
     return x >= 0 && x < size && y >= 0 && y < size && board[x][y] == -1;
 }
 
-bool solveKTBacktracking(int x, int y, int move, int** &board, int size, int xMove[8], int yMove[8]){
+bool solveKTBacktracking(int x, int y, int move, int &total, int** &board, int size, int xMove[8], int yMove[8]){
     int nextX, nextY;
-    if (move == size * size) return true;
-    for (int i = 0; i < size; i++) {
+    if (move > size * size) return true;
+    for (int i = 0; i < 8; i++) {
         nextX = x + xMove[i];
         nextY = y + yMove[i];
         if (isSafeBacktracking(nextX, nextY, board, size)) {
             board[nextX][nextY] = move;
-            if (solveKTBacktracking(nextX, nextY, move + 1, board, size, xMove, yMove)) 
-                return true;
+            total++;
+            if (solveKTBacktracking(nextX, nextY, move + 1, total, board, size, xMove, yMove)) return true;
             else board[nextX][nextY] = -1;
         }
     }
     return false;
 }
 
-void printSolution(int** sol, int size){
-    for (int x = 0; x < size; x++) {
-        for (int y = 0; y < size; y++)
-            std::cout << " " << std::setw(2) << sol[x][y] << " ";
-        std::cout << "\n";
+void outputFile(int** board, int size, int x, int y, int total, std::chrono::duration<double> elapsed_seconds){
+    std::fstream f("20127370_backtrack.txt", std::fstream::out);
+    f << x << " " << y << " " << size << "\n";
+    f << total << "\n";
+    f << elapsed_seconds.count() << "\n";
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+            f << " " << std::setw(2) << board[i][j] << " ";
+        f << "\n";
     }
 }
  
-void backtrackKT(){
-    int size, x, y;
-    std::cout << "Size: ";
-    std::cin >> size;
+void backtrackKT(int argc, char* argv[]){
+    int size = std::atoi(argv[6]);
+    int x = std::atoi(argv[2]), y = std::atoi(argv[4]);
     int** board = new int*[size];
     for(int i = 0; i < size; i++){
         board[i] = new int[size];
@@ -41,17 +44,20 @@ void backtrackKT(){
             board[i][j] = -1;
         }
     }
-    std::cin >> x >> y;
 
+    int total = 1;
     int moveX[8] = {2, 1, -1, -2, -2, -1, 1, 2};
     int moveY[8] = {1, 2, 2, 1, -1, -2, -2, -1};
  
-    board[x][y] = 0;
- 
-    if (!solveKTBacktracking(x, y, 1, board, size, moveX, moveY)) {
-        std::cout << "Solution does not exist";
-    }
-    else printSolution(board, size);
+    board[x-1][y-1] = 1;
+    auto start = std::chrono::system_clock::now();
+    bool checkKT = solveKTBacktracking(y-1, x-1, 2, total, board, size, moveX, moveY);
+    auto end  = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "Time: " << elapsed_seconds.count() << "\n";
+    if (!checkKT) std::cout << "Solution does not exist";
+    else outputFile(board, size, x, y, total, elapsed_seconds);
+
     for(int i = 0; i < size; i++){
         delete[] board[i];
     }
@@ -59,6 +65,6 @@ void backtrackKT(){
 }
  
 int main(int argc, char* argv[]){
-    backtrackKT();
+    backtrackKT(argc, argv);
     return 0;
 }
